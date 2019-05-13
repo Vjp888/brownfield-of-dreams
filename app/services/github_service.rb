c05c6d94ad57bee09504487142b52a277c581320
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 14:02:59 -0600

    Breaks apart github service method calls to be shorter than requires character limit
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,30 +1,36 @@
+# frozen_string_literal: true
+
 class GithubService
   def initialize(filter)
     @token = filter[:token]
   end
 
   def get_email(github_handle)
-    conn = Faraday.new("https://api.github.com/users/#{github_handle}?access_token=#{@token}")
-    response  = conn.get
-    data = JSON.parse(response.body, symbolize_names: true)
+    conn = Faraday.new('https://api.github.com/users/'\
+      "#{github_handle}?access_token=#{@token}")
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def get_repos
-    conn = Faraday.new("https://api.github.com/user/repos?access_token=#{@token}&affiliation=owner")
-    response  = conn.get
+    conn = Faraday.new('https://api.github.com/user/repos?'\
+      "access_token=#{@token}&affiliation=owner")
+    response = conn.get
     data = JSON.parse(response.body, symbolize_names: true)
     data.take(5)
   end
 
   def get_following
-    conn = Faraday.new("https://api.github.com/user/following?access_token=#{@token}&affiliation=owner")
-    response  = conn.get
-    data = JSON.parse(response.body, symbolize_names: true)
+    conn = Faraday.new('https://api.github.com/user/following?'\
+      "access_token=#{@token}&affiliation=owner")
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def get_followers
-    conn = Faraday.new("https://api.github.com/user/followers?access_token=#{@token}&affiliation=owner")
-    response  = conn.get
-    data = JSON.parse(response.body, symbolize_names: true)
+    conn = Faraday.new('https://api.github.com/user/followers?'\
+      "access_token=#{@token}&affiliation=owner")
+    response = conn.get
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
