d27e24b7f62c04866dfb08b7f2018fee96f02a45
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-05-13 14:02:59 -0600

    moves redirect calls outside of conditionals and breaks out methods into private calls
    Co-authored-by: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- app/controllers/users/friends_controller.rb
+++ app/controllers/users/friends_controller.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
+
 class Users::FriendsController < ApplicationController
   def create
-    new_friend = User.find_by(github_name: params[:login])
     if new_friend
       current_user.friends.create(followed_user_id: new_friend.id)
       flash[:success] = "Successfully added #{params[:login]} as a friend"
-      redirect_to dashboard_path
     else
       flash[:error] = "#{params[:login]} is not a valid user"
-      redirect_to dashboard_path
     end
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def new_friend
+    User.find_by(github_name: params[:login])
   end
 end
